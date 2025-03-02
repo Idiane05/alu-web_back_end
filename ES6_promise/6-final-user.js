6621bbcc7ff2c4735638472e3e0a6e0cf140737a
Idiane05 <d.ingabire1@alustudent.com>
2025-03-02 15:06:59 +0000

    added

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-all-reject.js';
-import uploadPhoto from './5-all-reject.js';
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
 
 export default async function handleProfileSignup(
   firstName,
